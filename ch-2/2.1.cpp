56c0b5da99ae506eb8cee8ae2eb93aad17a6acd4
Smit Garala <smeetgarala6606@gmail.com>
2025-05-12 11:30:23 +0530

    Lab Work - 2.1  12 May 2025

---
+++ ch-2/2.1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a = 12, b = 5;
+    int c = 0;
+
+    cout << "Addition of " << a << " and " << b << " is: " << a + b << endl;
+    cout << "Subtraction of " << a << " and " << b << " is: " << a - b << endl;
+    cout << "Multiplication of " << a << " and " << b << " is: " << a * b << endl;
+    cout << "Division of " << a << " and " << b << " is: " << a / b << endl;
+    cout << "Modulus of " << a << " and " << b << " is: " << a % b << endl;
+
+    cout << "a + b = " << a + b << endl;
+    cout << "a - b = " << a - b << endl;
+    cout << "a * b = " << a * b << endl;
+    cout << "a / b = " << a / b << endl;
+    cout << "a % b = " << a % b << endl;
+}
